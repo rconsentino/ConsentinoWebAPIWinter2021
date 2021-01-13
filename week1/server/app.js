8bfc929a9488f476a88e59624339f7d86f48a79f
rconsentino <ryanconsentino@gmail.com>
2021-01-13 13:06:23 -0500

    server code

--- week1/server/app.js
+++ week1/server/app.js
@@ -1,7 +1,11 @@
 //app is the entry point of application
+var http = require('http')
 
-function sendMsg(){
-    console.log("Hello world???");
-}
+http.createServer(function(request, response){
+    //http header
+    response.writeHead(200,{'content-type':'text/plain'})
+    //send a response to the body of the html
+    response.end("Wouldn't you like to be a pepper too???")
+}).listen(3000)
 
-sendMsg();
+console.log("Server is running on port 3000")
